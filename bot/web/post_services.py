de0cb638f21d85f684f4a42cc8b8f614df7b1ce4
Egor Roganin <eroganin@bk.ru>
2022-10-23 13:55:07 +0300

    typing

--- bot/web/post_services.py
+++ bot/web/post_services.py
@@ -1,18 +1,17 @@
 import random
-from typing import Union
-from bot.domain.objects.entity import Message, Photo
+
 from bot.domain.objects.repository import Repository
 from bot.domain.storage.storage import Storage
 
 
 def get_entity(repository: Repository):
-    useless_entity = [entity for entity in repository.entities if not entity.usable]
+    useless_entity: list[repository.entity_class] = [entity for entity in repository.entities if not entity.usable]
     print(f'{repository.__class__.__name__} --> {len(useless_entity)}')
     if not useless_entity:
         repository.make_all_useless()
-        entity: Union[Photo, Message] = random.choice(repository.entities)
+        entity: [repository.entity_class] = random.choice(repository.entities)
     else:
-        entity: Union[Photo, Message] = random.choice(useless_entity)
+        entity: [repository.entity_class] = random.choice(useless_entity)
     entity.usable = True
     return entity.get_content()
 
